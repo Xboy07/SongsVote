d54fce50865a4d5e86f762cc52fafe4f273a99cf
username-xboybx <jeswanth012@gmail.com>
2025-05-31 10:24:45 +0530

    added cors config

--- backend/server.ts
+++ backend/server.ts
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://0.0.0.0/songdb';
 
-app.use(cors());
+app.use(cors({origin: '*'}));
 app.use(express.json());
 
 mongoose.connect(MONGO_URI)
